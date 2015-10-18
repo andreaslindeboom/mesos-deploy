cbdb498c5f7d05dc0b8e0c9d22164b33a7973881
Andreas Lindeboom <andreaslindeboom@mdmail.nl>
2015-10-18 12:57:28 +0200

    Preventing Zookeeper from running on Mesos slave nodes

--- packer/scripts/install/mesos-slave.sh
+++ packer/scripts/install/mesos-slave.sh
@@ -8,5 +8,5 @@ echo "deb http://repos.mesosphere.com/ubuntu trusty main" | \
 apt-get -y update
 apt-get -y install openjdk-7-jre-headless mesos
 
-# the Mesos package starts both a master and a slave process on boot by default, this disables the master
-echo manual | tee /etc/init/mesos-master.override
+# the Mesos package starts Mesos master and slave processes as well as Zookeper by default, this disables the master and Zookeeper
+echo manual | tee /etc/init/mesos-master.override | tee /etc/init/zookeeper.override
